a4de4d9c12db0934917a9a32e8a4affe19889835
limzhilit <g00440629@atu.ie>
2026-02-22 21:01:34 +0000

    init

---
+++ src/main/java/ie/atu/oopsem2week4/OopSem2Week4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oopsem2week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Week4Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OopSem2Week4Application.class, args);
+  }
+
+}
