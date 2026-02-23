80cd8109f6d2d27c09cd855ed2b0cc78841f896e
limzhilit <g00440629@atu.ie>
2026-02-23 11:13:53 +0000

    finished tutorial

---
+++ src/main/java/ie/atu/oopsem2week4/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package ie.atu.oopsem2week4.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  /**
+   * Handles validation exceptions; returns field‑specific error messages
+   */
+  @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+  public ResponseEntity<Map<String, String>>
+  handleValidationException(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+
+    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+      String fieldName = error.getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    }
+    return ResponseEntity.badRequest().body(errors);
+  }
+}
